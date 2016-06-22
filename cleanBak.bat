23ef04b0c988d98bb4ec19600d325b8c56d5bf77
BXDadmin <xinqi_s@neuedu.com>
2016-06-22 23:05:13 +0800

    start github repo with already files

---
+++ cleanBak.bat
@@ -0,0 +1,23 @@
+del *.bak
+
+del bxd\*.bak
+del bxd\design\*.bak
+del bxd\pic\*.bak
+del bxd\dfx\*.bak
+del bxd\download\*.bak
+
+del bxd\dfx\16mar00_1\*.bak
+del bxd\dfx\16mar00_2\*.bak
+del bxd\dfx\16mar31_1\*.bak
+del bxd\dfx\16mar31_2\*.bak
+
+del bxd\topicFrame\*.bak
+del bxd\topicFrame\design\*.bak
+del bxd\topicFrame\download\*.bak
+del bxd\topicFrame\frame\*.bak
+del bxd\topicFrame\frame\list*.bak
+del bxd\topicFrame\frame\other\*.bak
+del bxd\topicFrame\pic\*.bak
+
+echo cleanAllBak for DFX website, v0.4, Apr 05 2016, BXD and BBaG
+pause
